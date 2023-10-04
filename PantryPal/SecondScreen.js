d14d546a2bf5b4fa3011dff768826cced6030ac1
mbridgnell <mjbridgnell@csuchico.edu>
2023-10-04 11:06:11 -0700

    Added different screens, home, and grocerylist

---
+++ PantryPal/SecondScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+const SecondScreen = ({navigation}) => {
+    return (
+        <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        </View>
+    );
+};
+
+export default SecondScreen;
