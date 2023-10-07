095e45605c16b38380dbe9bd744a5e54cf4b7002
Bryan Brooks <bjbrooks@csuchico.edu>
2023-10-07 13:44:51 -0700

    Update Navigation Stack

--- PantryPal/App.tsx
+++ PantryPal/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import GroceryList from './GroceryList';
 import SecondScreen from './SecondScreen';
+import AddItem from './AddItem';
+import DeleteItem from './DeleteItem';
 
 const Stack = createStackNavigator();
 
@@ -14,10 +16,12 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName = "Pantry Pal">
-        <Stack.Screen name = "Pantry Pal" component = {HomeScreen} />
-        <Stack.Screen name = "Grocery List" component = {GroceryList} />
-        <Stack.Screen name = "Pantry" component = {SecondScreen} />
+      <Stack.Navigator initialRouteName="Pantry Pal">
+        <Stack.Screen name="Pantry Pal" component={HomeScreen} />
+        <Stack.Screen name="Grocery List" component={GroceryList} />
+        <Stack.Screen name="Pantry" component={SecondScreen} />
+        <Stack.Screen name="Add Item" component={AddItem} />
+        <Stack.Screen name="Delete Item" component={DeleteItem} />
       </Stack.Navigator>
     </NavigationContainer>
   );
