4093d3f047c6ce0d6bbea1ea551064fdec1defe8
Bryan Brooks <bjbrooks@csuchico.edu>
2023-11-07 23:42:21 -0800

    Updating Add and Edit screens

--- PantryPal/EditItem.js
+++ PantryPal/EditItem.js
@@ -1,3 +1,10 @@
+/*
+ * File: PantryPal/EditItem.js
+ * Description: This is the EditItem screen
+ * It allows the user to edit an item in the pantry.
+ * It is accessed from the Pantry screen.
+ */
+
 import React, {useEffect, useState } from 'react';
 import {
   View,
@@ -10,11 +17,12 @@ import {
   ImageBackground,
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-// This is the local storage methods
+// Storage functions
 import {
-  editItem,  
+  editItem,
+  loadItem,
 } from './Storage.ts';
-// This is for the route
+// This is for the route from the pantry screen
 import { useRoute } from '@react-navigation/native';
 // This is the snackbar
 import Snackbar from 'react-native-snackbar';
@@ -23,41 +31,35 @@ import styles from './Styles.js';
 // Load the background image
 import image from './Images/pantryimage.jpg';
 
-const EditItem = ({navigator}) => {
+const EditItem = ({navigaton}) => {
   // This the route to the item
   const route = useRoute();
   // This is the item to edit
-  const { item } = route.params?.data;
-
+  const itemName  = route.params.key;
   // These are the states for the input fields
   const[quantity, setQuantity] = useState(0);
-  const[datePurchased, setDatePurchased] = useState(item.itemData.datePurchased);
-  const[expirationDate, setExpirationDate] = useState(item.itemData.expirationDate);
-  const[inRefrigerator, setInRefrigerator] = useState(item.itemData.inRefrigerator);
-  const[inFreezer, setInFreezer] = useState(item.itemData.inFreezer);
-  const[inPantry, setInPantry] = useState(item.itemData.inPantry);
+  const[datePurchased, setDatePurchased] = useState(new Date());
+  const[expirationDate, setExpirationDate] = useState(new Date());
+  const[inRefrigerator, setInRefrigerator] = useState(false);
+  const[inFreezer, setInFreezer] = useState(false);
+  const[inPantry, setInPantry] = useState(false);
   const[errorMessage, setErrorMessage] = useState('');
   const[showDatePicker, setShowDatePicker] = useState(false);
-  const[pantryData, setPantryData] = useState([]);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [isEditModalVisible, setEditModalVisible] = useState(false);
+  const[itemData, setItemData] = useState([]);
+  // const [selectedItem, setSelectedItem] = useState(null);
+  // const [isEditModalVisible, setEditModalVisible] = useState(false);
 
-  const openEditModal = (item) => {
-    setSelectedItem(item);
-    setEditModalVisible(true);
-  };
-
-  const closeEditModal = () => {
-    setEditModalVisible(false);
-  };
-
+  // Screen Functions
+  // This is the function to show the date picker
   const showDatepicker = () => {
     setShowDatePicker(true);
   };
 
-  const EditItem = async () => {
-    setEditModalVisible(false);
-  }
+  // This to fetch the item data from the firebase
+  const fetchData = async () => {
+    const item = await loadItem(itemName);
+    setItemData(item);
+  };
 
   const updateItem = async () => {
     try {
@@ -70,10 +72,14 @@ const EditItem = ({navigator}) => {
         inFreezer,
         inPantry,
       );
-      Snackbar.show({
-        text: 'Item edited successfully',
-        duration: Snackbar.LENGTH_LONG,
-      });
+      // Reset the input fields
+      setDatePurchased(new Date());
+      setExpirationDate(new Date());
+      setQuantity(0);
+      setInRefrigerator(false);
+      setInFreezer(false);
+      setInPantry(false);
+
     } catch (error) {
       setErrorMessage(error.message);
       // Show the error on the snackbar
@@ -86,102 +92,65 @@ const EditItem = ({navigator}) => {
 
   // This is to handle the incoming data from the route
   useEffect(() => {
-    if (route.params?.item) {
-      setSelectedItem(route.params.item);
-    }
-  }, [route.params?.item]);
+    fetchData();
+  }, []);
 
-  const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => openEditModal(item)}>
-      <View style={styles.textContainer}>
-        <Text style={styles.text2}>Name: {item.key}</Text>
-        <Text style={styles.text3}>     Quantity: {item.quantity}</Text>
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.text3}>Date Purchased: {item.datePurchased}</Text>
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.text3}>Expiration Date: {item.expiration}</Text>
-      </View>
-      <View style={styles.textContainer2}>
-        <Text style={styles.text3}>Location of {item.key}</Text>
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.text3}>Refrigerator: {item.itemData.fridge ? 'Yes' : 'No'}</Text>
-        <Text style={styles.text3}>     Freezer: {item.itemData.freezer ? 'Yes' : 'No'}</Text>
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.text3}>Pantry: {item.itemData.pantry ? 'Yes' : 'No'}</Text>
-      </View>
-    </TouchableOpacity>
-  );
 
   return (
     <ImageBackground
       source={image}
       style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
     >
-      <View style = {styles.container}>
-        <FlatList
-          data={pantryData}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.key}
+      <View>
+        <Text> Editing Item: {itemName} </Text>
+        <TextInput
+          keyboardType='numeric'
+          placeholder='Enter a Quantity'
+          onChangeText={setQuantity}
+          value={quantity}
+          maxLength={5}
+          style={styles.textBox2}
         />
-
-        {/* Edit Item Modal */}
-        <Modal visible={isEditModalVisible}>
-          <View style={styles.textContainer}>
-            <Text style={styles.text}>Editing Item: {selectedItem.key}</Text>
-            <TextInput
-              keyboardType='numeric'
-              placeholder='Enter a Quantity'
-              onChangeText={setQuantity}
-              value={quantity}
-              maxLength={5}
-              style={styles.textBox2}
-            />
-            <Button title='Select Date Purchased' onPress={showDatepicker} />
-            <Text style={styles.status}>Date Purchased: {datePurchased.toDateString()}</Text>
-            {showDatePicker && (
-              <DateTimePicker
-                value={datePurchased}
-                mode='date'
-                display='default'
-                onChange={(_, selectedDate) => {
-                  setShowDatePicker(false);
-                  if (selectedDate) {
-                    setDatePurchased(new Date(selectedDate));
-                  }
-                }}
-              />
-            )}
-            <Button title='Select Expiration' onPress={showDatepicker} />
-            <Text style={styles.status}>Expires: {expirationDate.toDateString()}</Text>
-            {showDatePicker && (
-              <DateTimePicker
-                value={expirationDate}
-                mode='date'
-                display='default'
-                onChange={(_, selectedDate) => {
-                  setShowDatePicker(false);
-                  if (selectedDate) {
-                    setExpirationDate(new Date(selectedDate));
-                  }
-                }}
-              />
-            )}
-            <Button title='In Refrigerator' onPress={() => setInRefrigerator(!inRefrigerator)} />
-            <Text style={styles.status}>In Refrigerator?: {inRefrigerator ? 'Yes' : 'No'}</Text>
-            <Button title='Freezer' onPress={() => setInFreezer(!inFreezer)} />
-            <Text style={styles.status}>In Freezer?: {inFreezer ? 'Yes' : 'No'}</Text>
-            <Button title='Pantry' onPress={() => setInPantry(!inPantry)} />
-            <Text style={styles.status}>In Pantry: {inPantry ? 'Yes' : 'No'}</Text>
-            {/* Add form fields for editing item properties */}
-            <Button title='Save' onPress={editItem} />
-            <Text>     </Text>
-            <Button title='Cancel' onPress={closeEditModal} />
-          </View>
-        </Modal>
+        <Button title='Select Date Purchased' onPress={showDatepicker} />
+        <Text style={styles.status}>Date Purchased: {datePurchased.toDateString()}</Text>
+        {showDatePicker && (
+          <DateTimePicker
+            value={datePurchased}
+            mode='date'
+            display='default'
+            onChange={(_, selectedDate) => {
+              setShowDatePicker(false);
+              if (selectedDate) {
+                setDatePurchased(new Date (selectedDate));
+              }
+            }}
+          />
+        )}
+        <Button title='Select Expiration' onPress={showDatepicker} />
+        <Text style={styles.status}>Expires: {expirationDate.toDateString()}</Text>
+        {showDatePicker && (
+          <DateTimePicker
+            value={expirationDate}
+            mode='date'
+            display='default'
+            onChange={(_, selectedDate) => {
+              setShowDatePicker(false);
+              if (selectedDate) {
+                setExpirationDate(new Date(selectedDate));
+              }
+            }}
+          />
+        )}
+        <Button title='In Refrigerator' onPress={() => setInRefrigerator(!inRefrigerator)} />
+        <Text style={styles.status}>In Refrigerator?: {inRefrigerator ? 'Yes' : 'No'}</Text>
+        <Button title='Freezer' onPress={() => setInFreezer(!inFreezer)} />
+        <Text style={styles.status}>In Freezer?: {inFreezer ? 'Yes' : 'No'}</Text>
+        <Button title='Pantry' onPress={() => setInPantry(!inPantry)} />
+        <Text style={styles.status}>In Pantry: {inPantry ? 'Yes' : 'No'}</Text>
+        
+        <Button color={'green'} title='Save Changes' onPress={ async () => {
+          await updateItem();
+        }} />
       </View>
     </ImageBackground>
   );
